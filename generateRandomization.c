32db6fbc26a89b359889ad2cab12b602d80b509e
hoangsonld <ledinhhoangson316@gmail.com>
2017-12-30 17:12:48 +0700

    generate randomization done stage 1, sending file next commit

--- generateRandomization.c
+++ generateRandomization.c
@@ -1,6 +1,7 @@
 #include <time.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <stdio.h>	
+#include <string.h>
 
 // Check whether the generated random number is existing in the array random_number[] or not
 int checkDistinctiveNumber(int generated_number[], int number_of_generated_numbers, int random_number) {
@@ -19,11 +20,10 @@ int checkDistinctiveNumber(int generated_number[], int number_of_generated_numbe
 
 // Generate respectively number_of_random_numbers random numbers within range [starting_number, ending_number]  
 
-int generateRandomNumbersBetweenRange(int random_number[], int number_of_random_numbers, int starting_number, int ending_number) {
+int generateRandomNumbersBetweenRange(int generated_number[], int number_of_random_numbers, int starting_number, int ending_number) {
 
 	int index;
 	int number;
-	int generated_number[10];
 	int current_index;
 
 	srand(time(NULL));
@@ -37,7 +37,7 @@ int generateRandomNumbersBetweenRange(int random_number[], int number_of_random_
 		// Check whether the generated random number exists in the array random_number[] or not
 		if(checkDistinctiveNumber(generated_number, current_index, number)) {
 			generated_number[current_index] = number;
-			printf("%d\n", number);
+			printf("%d\n", generated_number[current_index]);
 			current_index++;
 		}
 
@@ -54,8 +54,47 @@ int generateRandomNumbersBetweenRange(int random_number[], int number_of_random_
 // 	int number_of_random_numbers = 8;
 // 	int starting_number = 1;
 // 	int ending_number = 16;
+// 	char character;
+// 	char *question_number;
+
+// 	char filename1[100];
+// 	char filename2[100];
 
 // 	generateRandomNumbersBetweenRange(random_number, number_of_random_numbers, starting_number, ending_number);
+
+// 	FILE *filePointer;
+// 	FILE *firstPointer, *secondPointer;
+
+// 	filePointer = fopen("training/test.txt", "a+");
+
+// 	firstPointer = fopen("training/training_1.txt", "r");
+// 	strcpy(filename1, strrchr("training/training_1.txt", '/') + 1);
+// 	printf("%s\n", filename1);
+// 	strtok(filename1, "_");
+// 	question_number = strtok(NULL, "\0");
+// 	sprintf(question_number, "%d", atoi(question_number));
+// 	printf("%s\n", question_number);
+
+
+// 	secondPointer = fopen("training/training_2.txt", "r");
+// 	strcpy(filename2, strrchr("training/training_2.txt", '/') + 1);
+// 	printf("%s\n", filename2);
+
+// 	while((character = fgetc(firstPointer)) != EOF) {
+// 		fputc(character, filePointer);
+// 	}
+
+// 	while((character = fgetc(secondPointer)) != EOF) {
+// 		fputc(character, filePointer);
+// 	}
+
+// 	fclose(firstPointer);
+// 	fclose(secondPointer);
+
+// 	fclose(filePointer);
+
+// 	//remove("training/test.txt");
+
 
 
 // 	return 0;
--- handleTrainingRequest.c
+++ handleTrainingRequest.c
@@ -1,9 +1,93 @@
+#include "generateRandomization.c"
 // sendTrainingQuestionsToUser(connecting_socket, clientAddress);
 // receiveTrainingAnswersFromUser(connecting_socket, clientAddress);
 // sendResultsBackToUser(connecting_socket, clientAddress);
 
 int sendTrainingQuestionsToUser(int connecting_socket, struct sockaddr_in clientAddress) {
 
+	// Make the sending file
+	int starting;
+	int ending;
+	int random_number[10];
+	char question_number[10];
+	int i;
+	char extension[] = ".txt";
+	char mode[] = "training_";
+	char filename[100];
+	char character[10];
+	char read_character;
+	char directory[100];
+
+	FILE *filePointer = NULL;
+	FILE *componentFilePointer = NULL;
+
+	starting = 1;
+	ending = 15;
+
+	generateRandomNumbersBetweenRange(random_number, 5, 1, 15);
+
+	filePointer = fopen("training/sending_file.txt", "w+");
+
+	for(i = 0; i < 5; i++) {
+		memset(filename, '\0', 100);
+		memset(character, '\0', 10);
+		memset(directory, '\0', 100);
+		strcpy(directory, "training/");
+		sprintf(question_number, "%d", random_number[i]);
+		strcat(filename, mode);
+		strcat(filename, question_number);
+		strcat(filename, extension);
+		printf("%s\n", filename);
+
+		sprintf(question_number, "%d", i+1);
+		// Append question number in the content of file
+
+		strcat(character, question_number);
+		strcat(character, ". ");
+		fprintf(filePointer, "%s", character);
+
+		strcat(directory, filename);
+		componentFilePointer = fopen(directory, "r+");
+
+
+		read_character = fgetc(componentFilePointer);
+
+		while(read_character != EOF) {
+			fputc(read_character, filePointer);
+			read_character = fgetc(componentFilePointer);
+		}
+
+		fprintf(filePointer, "======================================\n");
+
+		fclose(componentFilePointer);
+
+	}
+
+	fclose(filePointer);
+
+	// Send the file to user
+
 	
 
+
+
 }
+
+int main(int argc, char const *argv[])
+{
+	
+	sendTrainingQuestionsToUser();
+
+	return 0;
+}
+
+// int receiveTrainingAnswersFromUser(int connecting_socket, struct sockaddr_in clientAddress) {
+
+
+
+// }
+
+// int sendResultsBackToUser(int connecting_socket, struct sockaddr_in clientAddress) {
+
+
+// }
